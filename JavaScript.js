94ce9f733165df531a2b5b79e12c1d8c8f5ee102
Otávio Augusto <vedanajazz@gmail.com>
2022-05-12 20:51:09 -0300

    subindo itens

---
+++ JavaScript.js
@@ -0,0 +1,23 @@
+
+
+function validaUser(){
+let user = document.getElementById("username").value;
+let nome;
+console.log("Estou sendo chamado");
+
+
+
+
+if(nome == user){
+
+console.log("nome invalido");
+console.log(user);
+
+
+}else{
+    console.log("nome valido");
+    nome=user;
+
+}
+  
+}
